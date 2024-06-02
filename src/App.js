62bee75d332b902bab339c8fcf73da2ecc607413
Khyaati-5 <130528543+Khyaati-5@users.noreply.github.com>
2024-06-02 01:14:18 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Block Chain Asset tokenization sample project</h1>
+      <p>This is a sample project using React Framework</p>
       </header>
     </div>
   );
